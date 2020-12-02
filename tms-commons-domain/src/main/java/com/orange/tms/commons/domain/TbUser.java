839cdbe3c873103bc81c19af5b8c874ed20c2a9a
liujia <cavomv@gmail.com>
2020-12-02 19:14:37 +0800

    增加tbUser

---
+++ tms-commons-domain/src/main/java/com/orange/tms/commons/domain/TbUser.java
@@ -0,0 +1,163 @@
+package com.orange.tms.commons.domain;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "tb_user")
+public class TbUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 创建时间
+     */
+    private Date created;
+
+    /**
+     * 更新时间
+     */
+    private Date updated;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取用户名
+     *
+     * @return username - 用户名
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置用户名
+     *
+     * @param username 用户名
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 获取密码
+     *
+     * @return password - 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取手机号
+     *
+     * @return phone - 手机号
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * 设置手机号
+     *
+     * @param phone 手机号
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * 获取邮箱
+     *
+     * @return email - 邮箱
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置邮箱
+     *
+     * @param email 邮箱
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取创建时间
+     *
+     * @return created - 创建时间
+     */
+    public Date getCreated() {
+        return created;
+    }
+
+    /**
+     * 设置创建时间
+     *
+     * @param created 创建时间
+     */
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    /**
+     * 获取更新时间
+     *
+     * @return updated - 更新时间
+     */
+    public Date getUpdated() {
+        return updated;
+    }
+
+    /**
+     * 设置更新时间
+     *
+     * @param updated 更新时间
+     */
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+}
