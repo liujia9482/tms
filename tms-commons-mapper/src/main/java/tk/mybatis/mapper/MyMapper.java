0e3d87d02347e5741e6841bb180ccbc1d66e28ee
liujia <cavomv@gmail.com>
2020-12-02 15:27:52 +0800

    增加MyMapper文件

---
+++ tms-commons-mapper/src/main/java/tk/mybatis/mapper/MyMapper.java
@@ -0,0 +1,11 @@
+package tk.mybatis.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 该接口不能被扫描到，否则会出错
+ * */
+
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
