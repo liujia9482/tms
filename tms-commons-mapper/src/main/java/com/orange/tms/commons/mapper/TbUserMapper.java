b3c68cbc82e8862d8740082aff6e2c59c5b6f197
liujia <cavomv@gmail.com>
2020-12-02 19:15:14 +0800

    增加TbUserMapper

---
+++ tms-commons-mapper/src/main/java/com/orange/tms/commons/mapper/TbUserMapper.java
@@ -0,0 +1,7 @@
+package com.orange.tms.commons.mapper;
+
+import com.orange.tms.commons.domain.TbUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbUserMapper extends MyMapper<TbUser> {
+}
